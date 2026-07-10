5628e6bb7773d714f7ba1b477c4a89f0b3a70775
Jackie Li <Lijackie@umich.edu>
2026-07-10 18:57:38 -0400

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int i = 0;
+        int n = nums.size();
+
+        while (i < n) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                i++;
+                continue;
+            }
+            int index = nums[i] - 1;
+
+            if (nums[i] != nums[index]) {
+                swap(nums[i], nums[index]);
+            }
+            else {
+                i++;
+            }
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+};
